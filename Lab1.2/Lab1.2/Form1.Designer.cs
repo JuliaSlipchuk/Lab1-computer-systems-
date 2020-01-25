52aaa3aa729c37c5f04de25090480f7a70c012f3
JuliaSlipchuk <julia2015olex@gmail.com>
2020-01-25 12:38:21 +0200

    converter to base64

---
+++ Lab1.2/Lab1.2/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Lab1._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(388, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "You can encode text in base64";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(17, 50);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(383, 304);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(17, 374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Select file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(447, 50);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(739, 219);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(747, 290);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(189, 64);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Get result";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1416, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Lab1.2/Lab1.2/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1._2
+{
+    public partial class Form1 : Form
+    {
+        static string filePath;
+        static List<char> lettersInBase64;
+
+        public Form1()
+        {
+            lettersInBase64 = new List<char>() { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S'
+                                            , 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
+                                            , 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6'
+                                            , '7', '8', '9', '+', '/' };
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+                using (StreamReader reader = new StreamReader(filePath, Encoding.Default, true))
+                {
+                    richTextBox1.Clear();
+                    richTextBox1.Text = reader.ReadToEnd();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text.Length == 0)
+                MessageBox.Show("The text is missing");
+            string strInBase64 = GetStrInBase64();
+            richTextBox2.Text = strInBase64;
+        }
+
+        private string GetStrInBase64()
+        {
+            string strInBase64 = "";
+            List<byte> textInBytes = System.Text.Encoding.UTF8.GetBytes(richTextBox1.Text).ToList();
+            while (textInBytes.Count % 3 != 0)
+            {
+                textInBytes.Add(0);
+            }
+            List<string> bitsRes;
+            FillBitsArr(out bitsRes, textInBytes);
+            ConvertBitsToLetters(out strInBase64, bitsRes);
+            return strInBase64;
+        }
+
+        private void FillBitsArr(out List<string> bitsRes, List<byte> textInBytes)
+        {
+            bitsRes = new List<string>();
+            for (int i = 0; i < textInBytes.Count; i += 3)
+            {
+                for (int j = i; j < i + 3; j++)
+                {
+                    List<string> bits = new List<string>();
+                    int[] arrBitsRev = new int[16];
+                    int k;
+                    for (k = 0; textInBytes[j] > 0; k++)
+                    {
+                        arrBitsRev[k] = textInBytes[j] % 2;
+                        textInBytes[j] /= 2;
+                    }
+                    if (k < 8)
+                    {
+                        for (int p = 0; p < 8 - k; p++)
+                        {
+                            bits.Add("0");
+                        }
+                    }
+                    for (k = k - 1; k >= 0; k--)
+                    {
+                        bits.Add(arrBitsRev[k].ToString());
+                    }
+                    bitsRes.AddRange(bits);
+                }
+            }
+        }
+
+        private void ConvertBitsToLetters(out string strInBase64, List<string> bitsRes)
+        {
+            strInBase64 = "";
+            for (int i = 0; i < bitsRes.Count; i += 6)
+            {
+                int counter = 5;
+                int res = 0;
+                for (int j = i; j < i + 6; j++)
+                {
+                    if (bitsRes[j] == "1")
+                    {
+                        res += (int)Math.Pow(2, counter);
+                    }
+                    counter--;
+                }
+                strInBase64 += lettersInBase64[res];
+            }
+        }
+    }
+}
